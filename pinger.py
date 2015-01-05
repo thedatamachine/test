2c77c1983554fc08eca37ae3dc2136030d0747be
Sean Findley <findley@me.com>
2015-01-05 15:56:42 -0800

    A little different approach.

--- pinger.py
+++ pinger.py
@@ -9,21 +9,18 @@ validip = re.search(
 extractRange = re.match(r'\d*.\d*.\d*.(\d{1,3})-(\d{1,3})', ip, re.IGNORECASE)
 extractTriplet = re.match(r'\d{1,3}.\d{1,3}.\d{1,3}.', ip, re.IGNORECASE)
 triplet = extractTriplet.group()  # extracted groups
-range1 = extractRange.group(1)
-range2 = extractRange.group(2)
-print "variable triplet = %s" % (triplet)
-print "variable range1 = %s" % (range1)
-print "variable range2 = %s\n" % (range2)
+range1 = int(extractRange.group(1))
+range2 = int(extractRange.group(2))
+print "triplet = %s" % (triplet)
+print "range1 = %s" % (range1)
+print "range2 = %s\n" % (range2)
 print "list of IP's to ping:"
-range1 = int(range1) - 1  # allows full range for list
 iplist = []
-while int(range1) < int(range2):
-    range1 = int(range1) + 1
-    total = str(triplet) + str(range1)
-    iplist.append(total)
+while range1 <= range2:
+    iplist.append(str(triplet) + str(range1))
+    range1 += 1
 print iplist
 for i in iplist:
     p = subprocess.Popen(["ping", "-c 1", "-W .2", i], stdout=subprocess.PIPE)
     line = p.communicate()
     print(line)
-# testpushers
